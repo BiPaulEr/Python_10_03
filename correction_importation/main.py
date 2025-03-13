07a9eb65d37bd503f64c1cf8ec5f0b2370354777
PaulErnest <PaulErMartin@yahoo.fr>
2025-03-13 15:37:02 +0100

    correction importation

---
+++ correction_importation/main.py
@@ -0,0 +1,5 @@
+import module as m
+from module import nom as NOM
+m.bonjour()
+
+print(NOM)
---
+++ correction_importation/module.py
@@ -0,0 +1,7 @@
+nom = "Paul"
+
+def bonjour():
+    print("Bonjour", nom)
+
+if __name__ == "__main__":
+    bonjour()
